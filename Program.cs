aba426f4a236341ba0d8ffd0638541923a2cf476
ArrowThief <33704630+ArrowThief@users.noreply.github.com>
2019-11-21 16:06:49 -0600

    Commit 1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Giraffe_2 {
+	class Program {
+		static void Main(string[] args) {
+			int core = findcore();
+
+			for (int i = 0; core > i; i++) {
+				Console.WriteLine("Yay!!");
+			}
+			//Console.ReadLine();
+
+		}
+
+		static int findcore() {
+			int hcore = Environment.ProcessorCount;
+			int core = hcore / 4;
+			return core;
+			//bob
+		}
+
+		static void Render() {
+			Process.Start("C:\\");
+		}
+	}
+
+
+
+}
+
---
+++ movie.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Giraffe_2
+{
+    class movie
+    {
+        public string title;
+        public string director;
+        private string rating;
+        public static string bob = "bob";
+
+        public movie(string title, string director, string rating)
+        {
+            this.title = title;
+            this.director = director;
+            this.Rating = rating;
+        }
+
+        public bool Goodforkids()
+        {
+            if(this.Rating == "R" || this.Rating == "PG-13")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public string Rating
+        {
+            get { return rating; }
+            set { 
+                  if (value == "G"||value == "PG" || value == "PG-13" || value == "R")
+                {
+                    rating = value;
+                }else
+                {
+                    rating = "NR";
+                }
+                }
+        }
+    }
+}
