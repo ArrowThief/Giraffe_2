b4cc47258665a007dc0a2c48a8ea68204da22257
ArrowThief <33704630+ArrowThief@users.noreply.github.com>
2020-01-02 15:09:40 -0600

    Updated to reduce render instances on older AMD processers, and cleaned up console writes.

--- Program.cs
+++ Program.cs
@@ -14,9 +14,9 @@ namespace Giraffe_2 {
 			Console.WriteLine("Rendering with " + dir.Substring(29));
 			dir = search() + "\\Support Files\\AErender.exe";
 			int core = findcore();
-			
 
-			for (;;)
+
+			for (; ; )
 			{
 				var updated1 = System.IO.File.GetLastWriteTime("M:\\Render Watch folders\\AFX Render file\\AFX Render_1.aep");
 				var updated2 = System.IO.File.GetLastWriteTime("M:\\Render Watch folders\\AFX Render file\\AFX Render_2.aep");
@@ -25,6 +25,7 @@ namespace Giraffe_2 {
 				TimeSpan Render_1 = crtime - updated1;
 				TimeSpan Render_2 = crtime - updated2;
 				TimeSpan maxtime = new TimeSpan(0, 0, 30, 0);
+				Console.Clear();
 				Console.WriteLine("Time since Render_1 was saved: " + Render_1);
 				Console.WriteLine("Time since Render_2 was saved: " + Render_2);
 
@@ -32,12 +33,13 @@ namespace Giraffe_2 {
 
 
 				if (maxtime >= Render_1 && maxtime >= Render_2) {
-				Process[] processes = new Process[core];
-				Process[] processes2 = new Process[core];
-				
-				Console.WriteLine("Starting Render_1");
-				for (int i = 0; core > i; i++) {
-					
+					Process[] processes = new Process[core];
+					Process[] processes2 = new Process[core];
+					Console.Clear();
+					Console.WriteLine("Starting Render_1 and Render_2, begining with Render_1");
+					Console.WriteLine(String.Format("Launching {0} Instaces of AfterFX", core));
+					for (int i = 0; core > i; i++) {
+
 						ProcessStartInfo info = new ProcessStartInfo();
 						info.UseShellExecute = true;
 						info.FileName = dir;
@@ -46,36 +48,40 @@ namespace Giraffe_2 {
 						processes[i] = Process.Start(info);
 						// processes.StartInfo = info;
 						// Process Run = Process.Start(info);
-						Console.WriteLine(String.Format("Yay!! There are {0} cores!", core));
+
 					}
 
 					Thread.Sleep(12000);
 
-					while(true) {
+					while (true) {
 						bool doBreak = true;
 						string usage = cpuCounter.NextValue().ToString("0");
 						Thread.Sleep(1000);
 						usage = cpuCounter.NextValue().ToString("0");
-						Console.WriteLine("CPU usage:"+usage+"%");
+						Console.Clear();
+						Console.WriteLine("CPU usage:" + usage + "%");
 						int usageD = Convert.ToInt16(usage);
+						int breaker =0;
 
-							foreach (Process process in processes) {
-								if (!process.HasExited) {
-									doBreak = false;
-									break;
-								}
+						foreach (Process process in processes) {
+							if (!process.HasExited) {
+								doBreak = false;
+								break;
 							}
+						}
 
-							if (doBreak) break;
-							else if (usageD <= 5) break;						
-							else Thread.Sleep(9000);
-							usage = cpuCounter.NextValue().ToString("0");
-							Thread.Sleep(1000);
-							usage = cpuCounter.NextValue().ToString("0");
-						
+						if (doBreak) break;
+						else if (usageD <= 5) breaker++;
+						else if (breaker == 10) break;
+						else Thread.Sleep(9000);
+						usage = cpuCounter.NextValue().ToString("0");
+						Thread.Sleep(1000);
+						usage = cpuCounter.NextValue().ToString("0");
+
 					}
+					Console.Clear();
 					Console.WriteLine("Starting Render_2");
-					for (int i = 0; core > i; i++) {	
+					for (int i = 0; core > i; i++) {
 						ProcessStartInfo info = new ProcessStartInfo();
 						info.UseShellExecute = true;
 						info.FileName = dir;
@@ -84,42 +90,45 @@ namespace Giraffe_2 {
 						processes2[i] = Process.Start(info);
 						// processes.StartInfo = info;
 						// Process Run = Process.Start(info);
-						Console.WriteLine(String.Format("Yay!! There are {0} cores!", core));
+						//Console.WriteLine(String.Format("Yay!! There are {0} cores!", core));
 					}
 
 					Thread.Sleep(12000);
 
-					while(true) 
-					{
+					while (true) {
 						bool doBreak2 = true;
 						string usage = cpuCounter.NextValue().ToString("0");
 						Thread.Sleep(1000);
 						usage = cpuCounter.NextValue().ToString("0");
-						Console.WriteLine("CPU usage:"+usage+"%");
+						Console.Clear();
+						Console.WriteLine("CPU usage:" + usage + "%");
 						int usageD = Convert.ToInt16(usage);
+						int breaker = 0;
 
-							foreach (Process process2 in processes2) {
-								if (!process2.HasExited) {
-									doBreak2 = false;
-									break;
-								}
+						foreach (Process process2 in processes2) {
+							if (!process2.HasExited) {
+								doBreak2 = false;
+								break;
 							}
+						}
 
-								if (doBreak2) break;
-								else if (usageD <= 10) break;						
-								else Thread.Sleep(9000);
-								usage = cpuCounter.NextValue().ToString("0");
-								Thread.Sleep(1000);
-								usage = cpuCounter.NextValue().ToString("0");
+						if (doBreak2) break;
+						else if (usageD <= 5) breaker++;
+						else if (breaker == 10) break;
+						else Thread.Sleep(9000);
+						usage = cpuCounter.NextValue().ToString("0");
+						Thread.Sleep(1000);
+						usage = cpuCounter.NextValue().ToString("0");
 					}
-				}	
-				else if (maxtime >= Render_1)
-				{
-				Process[] processes = new Process[core];
-				Console.WriteLine("Starting Render_1");
-				
-				for (int i = 0; core > i; i++) {
-					
+				} else if (maxtime >= Render_1) {
+					Console.Clear();
+					Console.WriteLine("Starting Render_1");
+					Console.WriteLine(String.Format("Launching {0} Instaces of AfterFX", core));
+					Process[] processes = new Process[core];
+					Console.WriteLine("Starting Render_1");
+
+					for (int i = 0; core > i; i++) {
+
 						ProcessStartInfo info = new ProcessStartInfo();
 						info.UseShellExecute = true;
 						info.FileName = dir;
@@ -128,41 +137,45 @@ namespace Giraffe_2 {
 						processes[i] = Process.Start(info);
 						// processes.StartInfo = info;
 						// Process Run = Process.Start(info);
-						Console.WriteLine(String.Format("Yay!! There are {0} cores!", core));
+						//Console.WriteLine(String.Format("Yay!! There are {0} cores!", core));
 					}
 
 					Thread.Sleep(12000);
 
-					while(true) {
+					while (true) {
 						bool doBreak = true;
 						string usage = cpuCounter.NextValue().ToString("0");
 						Thread.Sleep(1000);
 						usage = cpuCounter.NextValue().ToString("0");
-						Console.WriteLine("CPU usage:"+usage+"%");
+						Console.Clear();
+						Console.WriteLine("CPU usage:" + usage + "%");
 						var usageD = Convert.ToInt16(usage);
+						int breaker = 0;
 
-							foreach (Process process in processes) {
-								if (!process.HasExited) {
-									doBreak = false;
-									break;
-								}
+						foreach (Process process in processes) {
+							if (!process.HasExited) {
+								doBreak = false;
+								break;
 							}
+						}
 
-							if (doBreak) break;
-							else if (usageD <= 5) break;						
-							else Thread.Sleep(9000);
-							usage = cpuCounter.NextValue().ToString("0");
-							Thread.Sleep(1000);
-							usage = cpuCounter.NextValue().ToString("0");
-						
-					} 	
-				}
-				else if (maxtime >= Render_2)
-				{
-				Process[] processes2 = new Process[core];
-				Console.WriteLine("Starting Render_2");
+						if (doBreak) break;
+						else if (usageD <= 5) breaker++;
+						else if (breaker == 10) break;
+						else Thread.Sleep(9000);
+						usage = cpuCounter.NextValue().ToString("0");
+						Thread.Sleep(1000);
+						usage = cpuCounter.NextValue().ToString("0");
 
-					for (int i = 0; core > i; i++) {	
+					}
+				} else if (maxtime >= Render_2) {
+					Console.Clear();
+					Console.WriteLine("Starting Render_2");
+					Console.WriteLine(String.Format("Launching {0} Instaces of AfterFX", core));
+					Process[] processes2 = new Process[core];
+					Console.WriteLine("Starting Render_2");
+
+					for (int i = 0; core > i; i++) {
 						ProcessStartInfo info = new ProcessStartInfo();
 						info.UseShellExecute = true;
 						info.FileName = dir;
@@ -171,41 +184,39 @@ namespace Giraffe_2 {
 						processes2[i] = Process.Start(info);
 						// processes.StartInfo = info;
 						// Process Run = Process.Start(info);
-						Console.WriteLine(String.Format("Yay!! There are {0} cores!", core));
+						//Console.WriteLine(String.Format("Yay!! There are {0} cores!", core));
 					}
 
 					Thread.Sleep(12000);
 
-					while(true) 
-					{
+					while (true) {
 						bool doBreak2 = true;
 						string usage = cpuCounter.NextValue().ToString("0");
 						Thread.Sleep(1000);
 						usage = cpuCounter.NextValue().ToString("0");
-						Console.WriteLine("CPU usage:"+usage+"%");
+						Console.WriteLine("CPU usage:" + usage + "%");
 						int usageD = Convert.ToInt16(usage);
 
-							foreach (Process process2 in processes2) {
-								if (!process2.HasExited) {
-									doBreak2 = false;
-									break;
-								}
+						foreach (Process process2 in processes2) {
+							if (!process2.HasExited) {
+								doBreak2 = false;
+								break;
 							}
+						}
 
-								if (doBreak2) break;
-								else if (usageD <= 10) break;						
-								else Thread.Sleep(9000);
-								usage = cpuCounter.NextValue().ToString("0");
-								Thread.Sleep(1000);
-								usage = cpuCounter.NextValue().ToString("0");
+						if (doBreak2) break;
+						else if (usageD <= 5) break;
+						else Thread.Sleep(9000);
+						usage = cpuCounter.NextValue().ToString("0");
+						Thread.Sleep(1000);
+						usage = cpuCounter.NextValue().ToString("0");
 					}
-				}
-				else {
+				} else {
 					Thread.Sleep(9000);
 					string usage = cpuCounter.NextValue().ToString("0");
 					Thread.Sleep(1000);
 					usage = cpuCounter.NextValue().ToString("0");
-					Console.WriteLine("CPU usage:"+usage + "%");
+					Console.WriteLine("CPU usage:" + usage + "%");
 				}
 			}
 		}
@@ -238,10 +249,16 @@ namespace Giraffe_2 {
 		static int findcore() {
 			int hcore = Environment.ProcessorCount;
 			int core = hcore / 4;
-			return core;
+			string name = System.Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER");
+			//Console.WriteLine(name);
+			if (name == "AMD64 Family 21 Model 1 Stepping 2, AuthenticAMD") {
+				return 4;
+			} else {
+				return core;
+			}
 		}
 
-}
+	}
 }
 
 
